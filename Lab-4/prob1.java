f4924f7fb300f0d0ce58d79ee7aa0b496294d2e1
Aravind G <aravindganesan492004@gmail.com>
2023-11-28 16:42:44 +0530

    Add files via upload

---
+++ Lab-4/prob1.java
@@ -0,0 +1,10 @@
+class prob1 {
+	String name;
+	int rollno;
+	prob1() {
+		System.out.println("Object created using constructor");
+}
+	public static void main(String args[]) {
+		prob1 obj = new prob1();
+}
+}
