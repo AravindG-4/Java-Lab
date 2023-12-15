179b1f87d8a44803b923d5aabbb20c51484f67d4
AravindG-4 <aravindganesan492004@gmail.com>
2023-12-15 10:59:07 +0530

    Updated

--- Lab4/prob6.java
+++ Lab4/prob6.java
@@ -5,10 +5,14 @@ public class prob6 {
     void printSum(int a,int b, int c) {
         System.out.println("Sum "+(a+b+c));
     }
+    void printSum(double a,double b) {
+        System.out.println("Sum "+(a+b));
+    }
 
     public static void main(String[] args) {
         prob6 obj = new prob6();
         obj.printSum(1,2);
         obj.printSum(1,2,3);
+        obj.printSum(5.5,7.5);
     }
 }
